a472c59365a5582fe8012a0d9de7a48ae843d25f
guillaume.giamarchi@gmail.com <guillaume.giamarchi@gmail.com>
2013-04-05 18:41:27 +0200

    Ant task (minimal version)

--- i18n-ant/src/main/java/com/github/ggiamarchi/i18n/generator/ant/I18NGeneratorTask.java
+++ i18n-ant/src/main/java/com/github/ggiamarchi/i18n/generator/ant/I18NGeneratorTask.java
@@ -3,11 +3,37 @@ package com.github.ggiamarchi.i18n.generator.ant;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
 
+import com.github.ggiamarchi.i18n.generator.GeneratorLauncher;
+
 public class I18NGeneratorTask extends Task {
 	
-	@Override
-	public void execute() throws BuildException {
-		super.execute();
+	private String bundle;
+	private String dir;
+	private String output;
+	
+	public void setBundle(String bundle) {
+		this.bundle = bundle;
 	}
 
+	public void setDir(String dir) {
+		this.dir = dir;
+	}
+
+	public void setOutput(String output) {
+		this.output = output;
+	}
+
+	@Override
+	public void execute() throws BuildException {
+		System.out.println(bundle);
+		System.out.println(dir);
+		System.out.println(output);
+		
+		GeneratorLauncher launcher = new GeneratorLauncher();
+		
+		launcher.execute(bundle, null, null, null, new String [] {dir}, output);
+
+		
+	}
+	
 }
