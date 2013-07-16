fbe2ca63b4e67c453fa495afc4382df7785674c4
guillaume.giamarchi@gmail.com <guillaume.giamarchi@gmail.com>
2013-07-16 15:21:05 +0200

    Implement ant task + tests

--- i18n-generator-ant/src/main/java/com/github/ggiamarchi/i18n/generator/ant/I18NGeneratorTask.java
+++ i18n-generator-ant/src/main/java/com/github/ggiamarchi/i18n/generator/ant/I18NGeneratorTask.java
@@ -9,6 +9,8 @@ public class I18NGeneratorTask extends Task {
 	
 	private String bundle;
 	private String dir;
+	private String interfaceName;
+	private String className;
 	private String output;
 	
 	public void setBundle(String bundle) {
@@ -19,21 +21,31 @@ public class I18NGeneratorTask extends Task {
 		this.dir = dir;
 	}
 
+	public void setInterfaceName(String interfaceName) {
+		this.interfaceName = interfaceName;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
 	public void setOutput(String output) {
 		this.output = output;
 	}
 
 	@Override
 	public void execute() throws BuildException {
-		System.out.println(bundle);
-		System.out.println(dir);
-		System.out.println(output);
-		
+		if (bundle == null || bundle.isEmpty()) {
+			throw new BuildException("'bundle' property must be set for 'i18n-generator' task");
+		}
+		if (dir == null || dir.isEmpty()) {
+			throw new BuildException("'dir' property must be set for 'i18n-generator' task");
+		}
+		if (output == null || output.isEmpty()) {
+			throw new BuildException("'output' property must be set for 'i18n-generator' task");
+		}
 		GeneratorLauncher launcher = new GeneratorLauncher();
-		
-		launcher.execute(bundle, null, null, null, new String [] {dir}, output);
-
-		
+		launcher.execute(bundle, interfaceName, className, null, new String [] { getProject().getBaseDir() + "/" + dir}, getProject().getBaseDir() + "/" + output);
 	}
-	
+
 }
